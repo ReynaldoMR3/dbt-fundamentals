d9d99982a241f7baa5a7dae5886e97753e09f669
Reynaldo Méndez Robles <reynaldomendezr3@gmail.com>
2022-03-19 07:44:50 +0000

    updating practice with exemplar code

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,26 +1,30 @@
-with payments as (
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
+
+payments as (
     select * from {{ ref('stg_payments') }}
 ),
 
-orders as (
-    select * from {{ ref('stg_orders')}}
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
 ),
 
+final as (
 
-fact_orders as (
     select
-          orders.order_id
-        , orders.customer_id
-        , sum(payments.amount) as amount
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
     from orders
-    left join 
-        payments 
-    on
-        orders.order_id = payments.orderid
-    group by
-        orders.order_id
-        , orders.customer_id
+    left join order_payments using (order_id)
 )
 
-
-select * from fact_orders
+select * from final
